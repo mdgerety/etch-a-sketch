f4c7cce01258de024b99195bdd510e8a8506422b
Michael Gerety <37129324+mdgerety@users.noreply.github.com>
2021-01-13 16:23:53 -0800

    complete basic app

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 const dimensions = prompt("How many squares per column/row?");
 const sketchContainer = document.querySelector(".sketch-container");
+const clearBtn = document.querySelector('.btn-clear');
+
 
 function generateGrid(dimensions) {
     const e = document.querySelector('.sketch-container');
-    for (let i = 0; i < dimensions; i++) {
-        
-        let row = document.createElement("div");
-        row.className = "row";
-        for (let i = 0; i < dimensions; i++) {
-            let box = document.createElement("div");
-            box.className = "box";
-            row.appendChild(box);
-        }
-        e.appendChild(row); 
+    for (let i = 0; i < dimensions * dimensions; i++) {
+        let box = document.createElement("div");
+        box.className = "box";
+        box.addEventListener("mouseover", changeColor);
+        e.appendChild(box);
     }
 
-    e.style.gridTemplateRows = `repeat(${dimensions}, 20px)`;
-    e.style.gridTemplateColumns = `repeat(${dimensions}, 20px)`;
-    
 
+    e.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`;
+    e.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`;
+    
 }
+
+clearBtn.addEventListener('click', () => {
+    clearSketch();
+}) 
+
+
+function changeColor(e) {
+    e.target.classList.add("dark");
+}
+
+function clearSketch() {
+    const grid = document.querySelectorAll(".box");
+    grid.forEach((square) => {
+        square.classList.remove("dark")
+    });
+}
+
+    
 
     
     
